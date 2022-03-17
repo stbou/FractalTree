03aef8a13deb2763922a10c882f89912bc04f672
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2022-03-17 17:30:21 -0400

    upload functional code

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,132 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	ofSetWindowTitle("L - SYSTEM");
+	ofSetBackgroundColor(31);
+
+	window_width = ofGetWindowWidth();
+	window_height = ofGetWindowHeight();
+
+	gui.setup();
+
+	gui.add(branch_ui.setup("Tree size", 250, 10, 250));
+	gui.add(expand_ui.setup("Decreasing branch", 2, 1, 200));
+	gui.add(branch_length_ui.setup("Branch's length", 0.69, 0.4, 0.69));
+	gui.add(theta_ui.setup("Theta", 40, 1, 100));
+	gui.add(line_ui.setup("Line weight", 10, 0, 10));
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
+	gui.draw();
+
+	camera.begin();
+	camera.setVFlip(true);
+
+	ofRotate(ofGetElapsedTimef() * 10, 0, 1, 0);
+
+	ofTranslate(window_width / 12, window_height / 2, 0);
+	branch(branch_ui, line_ui);
+
+	camera.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::branch(float len, int line) {
+
+	ofSetLineWidth(line - 1);
+	ofDrawLine(0, 0, 0, -len);
+	ofTranslate(0, -len, 0);
+
+
+	len *= branch_length_ui;
+
+	if (len > expand_ui) {
+
+		ofPushMatrix();
+		ofRotateZDeg(theta_ui);
+		ofSetLineWidth(line - 1.5);
+		ofRotateYDeg(theta_ui);
+		branch(len, line - 1);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofRotateZDeg(-theta_ui);
+		ofSetLineWidth(line - 1.5);
+		branch(len, line - 1);
+		ofPopMatrix();
+
+		if (line < 5) {
+			ofSetColor(ofColor::green);
+		}
+		else {
+			ofSetColor(ofColor::saddleBrown);
+		}
+
+
+	}
+
+
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,47 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
+	void branch(float len, int line);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	// Gui panel 
+	ofxPanel gui;
+	ofxIntSlider branch_ui;
+	ofxIntSlider expand_ui;
+	ofxFloatSlider branch_length_ui;
+	ofxFloatSlider theta_ui;
+	ofxIntSlider line_ui;
+
+	// Camera 
+	ofEasyCam camera;
+
+	// Variables
+	int window_width;
+	int window_height;
+
+	float branch_length;
+	int branch_size;
+	float theta;
+	int expand;
+
+	int line_weight;
 };
