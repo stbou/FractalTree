11fd1f3f7c424b697ef143ef05228a6e0a3ffe63
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2022-03-17 18:07:32 -0400

    add comment and copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * \file main.cpp
+ * \author Stephane Boulanger
+ * \version 0.1
+ * \date 2022-03-17
+ * \copyright Copyright (c) 2022
+ */
+
 #include "ofMain.h"
 #include "ofApp.h"
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,3 +1,11 @@
+/**
+ * \file ofApp.h
+ * \author Stephane Boulanger
+ * \version 0.1
+ * \date 2022-03-17
+ * \copyright Copyright (c) 2022
+ */
+
 #pragma once
 
 #include "ofMain.h"
